da728b30f1c5771945f490790e599d5469fd9ec7
Nate Harper <nate.harper1@gmail.com>
2020-08-10 15:17:39 -0600

    Removed Old Tests

--- UVSim/UVSim/Main.cpp
+++ UVSim/UVSim/Main.cpp
@@ -20,15 +20,6 @@ int main(int argc, char* args[]) {
 	CLI* cli = CLI::getInstance(mem);
 	Interpreter* inter = Interpreter::getInstance(mem, alu);
 	// Start CLI
-	for (int i = 0; i < 10; ++i)
-	{
-		std::cout << "Add 1 +" << i << ": " << alu->Add(1, i) << std::endl;
-		std::cout << "Subtract 1000 -" << i << ": " << alu->Subtract(1000, i) << std::endl;
-		std::cout << "Divide 1000 /" << i+1 << ": " << alu->Divide(1000, i + 1) << std::endl;
-		std::cout << "Remainder 1000 %" << i+1 << ": " << alu->Remainder(1000, i + 1) << std::endl;
-		std::cout << "Exponent 1 " << i << ": " << alu->Exponent(1, i) << std::endl;
-		std::cout << "Multiply 2 *" << i << ": " << alu->Multiply(2, i) << std::endl;
-	}
 	cli->launch();
 	// Begin Computing
 	inter->run();
